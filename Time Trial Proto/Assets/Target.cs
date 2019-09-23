b8c86bc8d15645db05289d938f980189299af36a
ArtfulRoger23 <47542733+ArtfulRoger23@users.noreply.github.com>
2019-09-23 21:22:11 +0100

    Update from lab session 23.9.19
    Changes made in Hadi tutorial

--- Time Trial Proto/Assets/Target.cs
+++ Time Trial Proto/Assets/Target.cs
@@ -6,9 +6,10 @@ public class Target : MonoBehaviour
    
     public bool isEnemy = false;
 
-    public float delta = 1.5f;  // Amount to move left and right from the start point
+    public float delta = 0.1f;  // Amount to move left and right from the start point
     public float speed = 2.0f;
     private Vector3 startPos;
+
 
     void Start()
     {
@@ -16,10 +17,16 @@ public class Target : MonoBehaviour
     }
 
     void Update()
-    {
+    {/*
         Vector3 v = startPos;
         v.z += delta * Mathf.Sin(Time.time * speed);
-        transform.position = v;
+        transform.position = v;*/
+
+        Vector3 targetDirection = transform.forward;
+        float targetMagnitude = delta * Mathf.Sin(Time.time * speed);
+
+        transform.position += targetDirection * targetMagnitude;
     }
+
 
 }
