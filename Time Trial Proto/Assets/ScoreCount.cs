f86cab5eb69d39f9976544cd051c88cefdc8d30e
ArtfulRoger23 <47542733+ArtfulRoger23@users.noreply.github.com>
2019-09-23 21:17:58 +0100

    First Push

---
+++ Time Trial Proto/Assets/ScoreCount.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreCount : MonoBehaviour
+{
+
+    public int score = 0;
+
+    TextMeshProUGUI text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "Score: " + score;
+    }
+}
---
+++ Time Trial Proto/Assets/Shooting.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    private AudioSource mAudioSrc;
+    public float range = 100f;
+    public float impactForce = 100f;
+
+    ScoreCount scoreCount;
+
+    public Camera fpsCam;
+    
+
+
+    private void Start()
+    {
+        scoreCount = FindObjectOfType<ScoreCount>();
+        mAudioSrc = GetComponent<AudioSource>();
+    }
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            mAudioSrc.Play();
+            Shoot();
+        }
+
+
+
+    void Shoot()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+            {
+                Debug.Log(hit.transform.name);
+
+                Target target = hit.transform.GetComponent<Target>();
+
+                if (target != null)
+                {
+
+                    if (target.isEnemy == true)
+                    {
+                        scoreCount.score += 1;
+                    }
+
+                    if (target.isEnemy == false)
+                    {
+                        scoreCount.score -= 1;
+                    }
+
+                    hit.rigidbody.AddForce((hit.normal * impactForce) * -1);
+
+                    Destroy(target, 0.3f);
+                }
+            }
+        }
+    }
+
+}
