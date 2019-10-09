a5cf0ad71db99ef52170baebd21041756bc6a8fd
CALLUM ROGER <1704077@uad.ac.uk>
2019-10-09 17:35:27 +0100

    BIG UPDATE
    added more targets and kill pits

---
+++ Time Trial Proto/Assets/Respawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform spawn;
+
+    void OnTriggerEnter(Collider other)
+    {
+        other.transform.position = spawn.transform.position;
+        Debug.Log(other.transform.name);
+    }
+
+
+
+}
